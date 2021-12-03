838b367d35395adece35489cb29e1176a1c3646f
devopswizard <devopswizard@wizardcontroller.com>
2021-12-03 17:39:40 -0500

    added core interfaces asssembly

---
+++ src/web/TheHorselessNewspaper/Web.Core/IViewTemplateDeployment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Core
+{
+    /// <summary>
+    /// specifies a razor class library deployment
+    /// 
+    /// the dynamic type loader identifies assemblies that contain
+    /// razxor class libraries by looking for this meta data
+    /// </summary>
+    public interface IViewTemplateDeployment
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Publisher { get; set; }
+
+    }
+}
