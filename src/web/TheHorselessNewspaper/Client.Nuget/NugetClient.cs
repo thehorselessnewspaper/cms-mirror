1e9d4a318948e2a68607fb983b8f3cbf68e59c2d
devopswizard <devopswizard@wizardcontroller.com>
2021-12-04 15:06:59 -0500

    nuget package issues fix attempt

---
+++ src/web/TheHorselessNewspaper/Client.Nuget/NugetClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.Client.Nuget
+{
+    public class NugetClient : HorselessNewspaper.Web.Core.Interfaces.INugetProtocol
+    {
+    }
+}
