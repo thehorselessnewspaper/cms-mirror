5bfd5e8edbd5270ce7781ec2aa5fb4d3f40d3d0b
devopswizard <devopswizard@wizardcontroller.com>
2021-12-04 18:33:30 -0500

    refactoring interfaces

---
+++ src/web/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/Nuget/INugetProtocol.cs
@@ -0,0 +1,21 @@
+﻿using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.Core.Interfaces.Nuget
+{
+    /// <summary>
+    /// wrapper for the .net nuget client sdk
+    /// 
+    /// supports username/password authenticated feeds
+    /// </summary>
+    public interface INugetProtocol
+    {
+        Task<List<NuGetVersion>> ListPackageVersions(Uri repositoryUri, string nugetPackageId, string userName = "", string password = "");
+
+        Task<NuGetVersion> PersistNugetTolocalFilesystem(Uri repositoryUri, string nugetPackageId, NuGetVersion nugetVersion, string folderPath, string userName = "", string password = "");
+    }
+}
---
+++ src/web/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/ViewTemplate/IViewTemplateDeployment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.Core.Interfaces.ViewTemplate
+{
+    public interface IViewTemplateDeployment
+    {
+        public string PackageId { get; set; }
+
+        public string PackageTitle { get; set; }
+
+        public string PackagePublisher { get; set; }
+
+        public string PackageVersion { get; set; }
+    }
+}
