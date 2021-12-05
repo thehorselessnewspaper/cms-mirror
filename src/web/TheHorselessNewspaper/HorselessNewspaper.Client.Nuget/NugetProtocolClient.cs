f254c6246b863cd48288a975a8265a232c017b9a
devopswizard <devopswizard@wizardcontroller.com>
2021-12-05 12:50:04 -0500

    nuget package generation and consumption functional

---
+++ src/web/TheHorselessNewspaper/HorselessNewspaper.Client.Nuget/NugetProtocolClient.cs
@@ -0,0 +1,26 @@
+﻿using HorselessNewspaper.Core.Interfaces.Nuget;
+using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.Client.Nuget
+{
+    /// <summary>
+    /// supports asset deployment and management via nuget package
+    /// </summary>
+    public class NugetProtocolClient : INugetProtocol
+    {
+        public async Task<List<NuGetVersion>> ListPackageVersions(Uri repositoryUri, string nugetPackageId, string userName = "", string password = "")
+        {
+            return await Task.FromResult(new List<NuGetVersion>());
+        }
+
+        public async Task<NuGetVersion> PersistNugetTolocalFilesystem(Uri repositoryUri, string nugetPackageId, NuGetVersion nugetVersion, string folderPath, string userName = "", string password = "")
+        {
+            return await Task.FromResult(new NuGetVersion(""));
+        }
+    }
+}
