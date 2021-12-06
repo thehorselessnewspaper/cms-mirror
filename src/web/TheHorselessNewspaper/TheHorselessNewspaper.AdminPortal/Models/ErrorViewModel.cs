dc0d45520d23ff634ab71c4737e7074c4f52b54a
devopswizard <devopswizard@wizardcontroller.com>
2021-12-06 15:58:54 -0500

    sanity commit

---
+++ src/web/TheHorselessNewspaper/TheHorselessNewspaper.AdminPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheHorselessNewspaper.AdminPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
