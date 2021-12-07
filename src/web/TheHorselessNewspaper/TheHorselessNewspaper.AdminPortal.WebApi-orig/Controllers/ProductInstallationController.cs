2bddfc4f5a50f354e44dd78a0d46cb14e1044c1f
devopswizard <devopswizard@wizardcontroller.com>
2021-12-06 22:29:07 -0500

    ditto

---
+++ src/web/TheHorselessNewspaper/TheHorselessNewspaper.AdminPortal.WebApi-orig/Controllers/ProductInstallationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace TheHorselessNewspaper.AdminPortal.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+
+    public class ProductInstallationController : ControllerBase
+    {
+    }
+}
