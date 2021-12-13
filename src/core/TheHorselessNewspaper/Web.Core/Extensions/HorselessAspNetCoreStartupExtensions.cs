480087e0967855a4284ae759f2af5a55edf3acf2
devopswizard <devopswizard@wizardcontroller.com>
2021-12-13 16:08:34 -0500

    adding headless cms support

---
+++ src/core/TheHorselessNewspaper/Web.Core/Extensions/HorselessAspNetCoreStartupExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.Web.Core.Extensions
+{
+    /// <summary>
+    /// support headless cms asp.net core startup semantics
+    /// </summary>
+    public  static class HorselessAspNetCoreStartupExtensions
+    {
+        public static IServiceCollection AddHorselessNewspaper(this IServiceCollection services,
+        Action<HorselessServiceBuilder> options = null, ServiceLifetime scope = ServiceLifetime.Scoped)
+        {
+            var serviceBuilder = new HorselessServiceBuilder(services);
+
+            options?.Invoke(serviceBuilder);
+
+            return services;
+        }
+    }
+}
---
+++ src/core/TheHorselessNewspaper/Web.Core/Extensions/HorselessServiceBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HorselessNewspaper.Web.Core.Extensions
+{
+    public class HorselessServiceBuilder
+    {
+        /// <summary>
+        /// The inner Service collection.
+        /// </summary>
+        public readonly IServiceCollection Services;
+
+        /// <summary>
+        /// Gets/sets if runtime compilation should be enabled.
+        /// </summary>
+        public bool AddRazorRuntimeCompilation { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="services">The current service collection</param>
+        public HorselessServiceBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+    }
+}
