461fb4d212b3c197ab066a4935c5b02cf6a1044e
devopswizard <devopswizard@wizardcontroller.com>
2021-12-17 18:01:40 -0500

    scaffolding default razor class library

---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/Controllers/HorselessCMSController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HorselessNewspaper.RazorClassLibrary.CMS.Default.Controllers
+{
+    [Route("/")]
+    public class HorselessCMSController : Controller
+    {
+        public IActionResult ViewTemplate()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/Models/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.RazorClassLibrary.CMS.Default.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
