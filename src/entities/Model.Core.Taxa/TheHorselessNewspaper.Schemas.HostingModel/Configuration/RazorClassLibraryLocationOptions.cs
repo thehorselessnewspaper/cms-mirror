17cbc50ebc51ec5a726f60accc5aae6b8ab4e63a
devopswizard <devopswizard@wizardcontroller.com>
2021-12-17 23:20:04 -0500

    modelupdate

---
+++ src/entities/Model.Core.Taxa/TheHorselessNewspaper.Schemas.HostingModel/Configuration/RazorClassLibraryLocationOptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHorselessNewspaper.Schemas.HostingModel.Configuration
+{
+
+    internal class RazorClassLibraryLocationOption
+    {
+
+        public RazorClassLibraryLocationOption()
+        {
+
+        }
+
+        internal Uri ClassLibraryRootFolderUri { get; set; }
+
+        /// <summary>
+        /// useful for getting the locations of class libraries
+        /// </summary>
+        internal string ClassLibraryNameRootFolder { get; set; }
+
+        /// <summary>
+        /// useful for setting timer background task update intervals
+        /// </summary>
+        internal int FilesystemEnumerationUpdateInterval { get; set; }
+
+    }
+
+    /// <summary>
+    /// specifies filesystem details for locations of razor class libraries
+    /// </summary>
+    internal class RazorClassLibraryLocationOptions
+    {
+        internal RazorClassLibraryLocationOptions()
+        {
+            Locations = new List<RazorClassLibraryLocationOption>();
+        }
+
+        internal List<RazorClassLibraryLocationOption> Locations { get; set; }
+
+    }
+}
