d06fde4221e1590f57005e231a63b566e8a5199c
devopswizard <devopswizard@wizardcontroller.com>
2021-12-22 23:30:25 -0500

    upstream nuget build commit

--- src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/Model/ExtensionConfiguration.cs
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/Model/ExtensionConfiguration.cs
@@ -6,6 +6,31 @@ using System.Threading.Tasks;
 
 namespace HorselessNewspaper.Core.Interfaces.Model
 {
+    /// <summary>
+    /// support nuget target framework specification 
+    /// as per 
+    ///  https://docs.microsoft.com/en-us/dotnet/standard/frameworks
+    /// </summary>
+    public class TargetedFramework
+    {
+        private TargetedFramework() { }
+
+        public TargetedFramework(string value)
+        {
+            Value = value; 
+        }
+
+        /// <summary>
+        /// TODO prevent setting any but the list specified as constants here
+        /// </summary>
+        public string Value { get; set; } = string.Empty;
+
+        public const string NET60 = "net6.0";
+        public const string NET50 = "net5.0";
+        public const string NETCoreApp31 = "netcoreapp3.1";
+    }
+
+  
     public interface IExtensionConfiguration
     {
         public string Package { get; set; }
--- src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/Nuget/INugetProtocol.cs
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/Nuget/INugetProtocol.cs
@@ -33,7 +33,7 @@ namespace HorselessNewspaper.Core.Interfaces.Nuget
 
         public Task<NuGetVersion> PersistNugetTolocalFilesystem(Uri repositoryUri, string nugetPackageId, NuGetVersion nugetVersion, string folderPath);
         public Task<NuGetVersion> PersistNugetTolocalFilesystem(Uri repositoryUri, string nugetPackageId, NuGetVersion nugetVersion, string folderPath, INugetProtocolCredentials credentials);
-        public Task LoadExtensions(IEnumerable<PackageSource> packageSources, IEnumerable<IExtensionConfiguration> extensions, string nugetFrameworkParseFolder, string packageDirectory);
+        public Task LoadExtensions(IEnumerable<PackageSource> packageSources, IEnumerable<IExtensionConfiguration> extensions, TargetedFramework nugetFrameworkParseFolder, string packageDirectory);
 
 
     }
