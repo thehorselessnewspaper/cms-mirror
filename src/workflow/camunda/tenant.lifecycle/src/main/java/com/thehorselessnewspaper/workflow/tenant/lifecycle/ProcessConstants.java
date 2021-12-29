4e93910ddf99eea4b6712009cf2baf0eeab5cc9b
devopswizard <devopswizard@wizardcontroller.com>
2021-12-28 19:39:48 -0500

    integrating bpm with camunda

---
+++ src/workflow/camunda/tenant.lifecycle/src/main/java/com/thehorselessnewspaper/workflow/tenant/lifecycle/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.thehorselessnewspaper.workflow.tenant.lifecycle;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "tenant.lifecycle"; // BPMN Process ID
+
+}
---
+++ src/workflow/camunda/tenant.lifecycle/src/main/java/com/thehorselessnewspaper/workflow/tenant/lifecycle/TenantLifecycle.java
@@ -0,0 +1,9 @@
+package com.thehorselessnewspaper.workflow.tenant.lifecycle;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Tenant Lifecycle")
+public class TenantLifecycle extends ServletProcessApplication {
+
+}
