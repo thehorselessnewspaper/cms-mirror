76c9ae14e893d25cc12e773bfc98bde28e9a958a
devopswizard <devopswizard@wizardcontroller.com>
2022-01-01 01:33:23 -0500

    sanity commit

---
+++ src/core/TheHorselessNewspaper/Web.Core/SingletonServices/HorselessAuthorization/Content/RowLevelSecuredAuthorizationHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheHorselessNewspaper.Schemas.ContentModel.ContentEntities;
+using TheHorselessNewspaper.Schemas.HostingModel.Context;
+
+namespace HorselessNewspaper.Web.Core.SingletonServices.HorselessAuthorization.Content
+{
+    #region authentication requirements
+    public class IsMustBeAuthenticatedRequirement : IAuthorizationRequirement { }
+    #endregion
+
+    #region role requirements
+    public class IsOwnerRequirement : IAuthorizationRequirement { }
+    #endregion
+
+    #region row level secured requirements
+    public class IsNotDeniedRequirement : IAuthorizationRequirement { }
+
+    public class CanDeleteRequirement : IAuthorizationRequirement { }
+
+    public class CanExecuteRequirement : IAuthorizationRequirement { }
+
+    public class CanReadRequirement : IAuthorizationRequirement { }
+
+    public class CanShareRequirement : IAuthorizationRequirement { }
+
+    public class CanUpdateRequirement : IAuthorizationRequirement { }
+
+    public class CanWriteRequirement : IAuthorizationRequirement { }
+    #endregion
+    /// <summary>
+    /// integrate with asp.net core authorization 
+    /// as per
+    ///     - https://docs.microsoft.com/en-us/aspnet/core/security/authorization/resourcebased?view=aspnetcore-6.0
+    /// </summary>
+    public class DeleteOperationAuthorizationHandler : AuthorizationHandler<CanDeleteRequirement, IRowLevelSecured>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CanDeleteRequirement requirement, IRowLevelSecured resource)
+        {
+
+            // apply delete permission evaluation strategies
+
+                
+            return Task.CompletedTask;
+        }
+    }
+}
