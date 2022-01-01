05d19941f097d3b957cc6bc6a08ec241a0d0f2e3
devopswizard <devopswizard@wizardcontroller.com>
2021-12-31 19:10:26 -0500

    adding an odata entity data model generator

---
+++ src/entities/Model.Core.Taxa/TheHorselessNewspaper.Schemas.HostingModel/ODATA/HorselessOdataModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheHorselessNewspaper.Schemas.ContentModel.ContentEntities;
+
+namespace TheHorselessNewspaper.Schemas.HostingModel.ODATA
+{
+    public class HorselessOdataModel
+    {
+        public async Task<IEdmModel> GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<AccessControlEntry>("AccessControlEntries");
+
+            builder.EntitySet<ContentCollection>("ContentCollections");
+
+            builder.EntitySet<FilesystemAsset>("FilesystemAssets");
+
+            builder.EntitySet<Holonym>("Holonyms");
+
+            builder.EntitySet<JSONAsset>("JSONAssets");
+
+            builder.EntitySet<MIMEType>("MIMETypes");
+
+            builder.EntitySet<Meronym>("Meronyms");
+
+            builder.EntitySet<MimeContent>("MimeContents");
+
+            builder.EntitySet<NugetPackage>("NugetPackages");
+
+            builder.EntitySet<Placeholder>("Placeholders");
+
+            builder.EntitySet<Principal>("Principals");
+
+            builder.EntitySet<Publication>("Publications");
+
+            builder.EntitySet<Taxon>("Taxons");
+
+            builder.EntitySet<Tenant>("Tenants");
+
+            return await Task.FromResult(builder.GetEdmModel());
+        }
+
+    }
+}
+
