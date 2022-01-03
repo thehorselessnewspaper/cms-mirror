3a0f85fa53df8a25ca239e9600354bd9b67e315a
devopswizard <devopswizard@wizardcontroller.com>
2022-01-03 13:44:56 -0500

    upstream nuget commit

---
+++ src/entities/Model.Core.Taxa/TheHorselessNewspaper.Schemas.HostingModel/ContentEntities/Query/ContentCollections/ContentCollectionQueries.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheHorselessNewspaper.Schemas.ContentModel.ContentEntities;
+using TheHorselessNewspaper.Schemas.HostingModel.Context;
+
+namespace TheHorselessNewspaper.HostingModel.ContentEntities.Query.ContentCollections
+{
+    public class ContentCollectionQueries : IQueryableContentModelOperator<ContentCollection>
+    {
+        private readonly ILogger<ContentCollectionQueries> _logger;
+        private readonly IContentModelContext _context;
+        internal ContentCollectionQueries(IContentModelContext ctx, ILogger<ContentCollectionQueries> logger)
+        {
+            this._context = ctx;    
+            this._logger = logger;
+
+            try
+            {
+                var providerName = ((DbContext)ctx).Database.ProviderName;
+                _logger.LogDebug($"content collections context using provider named {providerName}");
+            }
+            catch (Exception e) { }
+        }
+
+        public async Task<IQueryable<ContentCollection>> Get()
+        {
+            _logger.LogDebug($"handling get request");
+            return await Task.FromResult<IQueryable<ContentCollection>>(_context.ContentCollections.AsQueryable<ContentCollection>());
+        }
+    }
+}
---
+++ src/entities/Model.Core.Taxa/TheHorselessNewspaper.Schemas.HostingModel/ContentEntities/Query/IQueryableContentModelOperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheHorselessNewspaper.Schemas.HostingModel.Context;
+
+namespace TheHorselessNewspaper.HostingModel.ContentEntities.Query
+{
+    /// <summary>
+    /// support arbitrary linq query composition
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IQueryableContentModelOperator<T> where T : IRowLevelSecured
+    {
+        public Task<IQueryable<T>> Get();
+    }
+}
