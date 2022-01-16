c41e9cf6a02901d944bc0410b18a1e9ab307f4d0
devopswizard <devopswizard@wizardcontroller.com>
2022-01-15 23:34:42 -0500

    can import api from @wizardcontroller/horseless-contentapi-lib

--- src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/horseless-tags/horseless-tags.module.ts
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/horseless-tags/horseless-tags.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HorselessTagsComponent } from './horseless-tags.component';
+import { ApiModule, Configuration, ConfigurationParameters } from '@wizardcontroller/horseless-contentapi-lib';
 
 @NgModule({
   imports: [
--- src/generated-code/angular/horseless-contentapi-workspace/projects/wizardcontroller/horseless-contentapi-lib/src/public-api.ts
+++ src/generated-code/angular/horseless-contentapi-workspace/projects/wizardcontroller/horseless-contentapi-lib/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/horseless-contentapi-lib.service';
 export * from './lib/horseless-contentapi-lib.component';
 export * from './lib/horseless-contentapi-lib.module';
+export * from './horseless-contentapi/api.module'
+export * from './horseless-contentapi/index'
