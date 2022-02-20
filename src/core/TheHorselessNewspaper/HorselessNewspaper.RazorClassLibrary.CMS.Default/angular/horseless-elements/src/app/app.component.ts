27b724c960f7e7d7aa69e1a7e81148b5aaa18ada
devopswizard <devopswizard@wizardcontroller.com>
2022-02-20 13:11:17 -0500

    can render <horseless-new-element/> with ng serve

--- src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/app.component.ts
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { NewTenantComponent } from './horseless-tags/new-tenant/new-tenant.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'horseless-elements';
+
+  constructor(injector: Injector) {
+    // Convert `PopupComponent` to a custom element.
+    const NewTenantElement = createCustomElement(NewTenantComponent, { injector });
+    // Register the custom element with the browser.
+    customElements.define('horseless-new-tenant', NewTenantElement);
+  }
 }
