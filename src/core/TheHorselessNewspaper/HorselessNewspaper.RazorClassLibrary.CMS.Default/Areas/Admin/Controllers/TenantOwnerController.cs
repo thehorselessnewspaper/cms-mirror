ec5f0416d0cc2abf17358904cd4d1bec5a9c2adf
devopswizard <devopswizard@wizardcontroller.com>
2022-03-08 01:30:53 -0500

    can insert new tenant for approval

--- src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/Areas/Admin/Controllers/TenantOwnerController.cs
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/Areas/Admin/Controllers/TenantOwnerController.cs
@@ -1,17 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Finbuckle.MultiTenant;
+using HorselessNewspaper.Web.Core.Interfaces.Content;
+using HorselessNewspaper.Web.Core.Interfaces.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TheHorselessNewspaper.HostingModel.ContentEntities.Query;
+using TheHorselessNewspaper.HostingModel.Entities.Query;
+using HostingModel = TheHorselessNewspaper.Schemas.HostingModel.HostingEntities;
+
+using ContentModel = TheHorselessNewspaper.Schemas.ContentModel.ContentEntities;
 
 namespace HorselessNewspaper.RazorClassLibrary.CMS.Default.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class TenantOwnerController : Controller
     {
-        public IActionResult Index()
+        public IContentCollectionService<IQueryableContentModelOperator<ContentModel.Tenant>, ContentModel.Tenant> tenantCollectionService { get; set; }
+        public IHostingCollectionService<IQueryableHostingModelOperator<HostingModel.Tenant>, HostingModel.Tenant> hostingTenantsCollectionService { get; set; }
+        public ITenantInfo CurrentTenant { get; set; }
+
+        ILogger<TenantOwnerController> logger;
+
+        public TenantOwnerController(
+            ILogger<TenantOwnerController> logger,
+            IContentCollectionService<IQueryableContentModelOperator<ContentModel.Tenant>, ContentModel.Tenant> tenantCollectionService,
+            IHostingCollectionService<IQueryableHostingModelOperator<HostingModel.Tenant>, HostingModel.Tenant> hostingTenantsCollectionService,
+            ITenantInfo tenantInfo
+            )
         {
+            this.tenantCollectionService = tenantCollectionService;
+            this.hostingTenantsCollectionService = hostingTenantsCollectionService;
+            this.CurrentTenant = tenantInfo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var iss = User.Claims.FirstOrDefault().Issuer;
+            var sub = User.Claims.FirstOrDefault().Subject.Name;
+
+            var hasUnpublishedTenantQuery = await hostingTenantsCollectionService.Query();
+            var hasWaitingRequest = hasUnpublishedTenantQuery
+                .Where(w => w.Owners.Where(o => o.Iss.Equals(iss) && o.Sub.Equals(sub)).Any()).Any();
+
+            ViewData["WaitingRequestMessage"] = hasWaitingRequest;
             return View();
         }
 
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
+            var iss = User.Claims.FirstOrDefault().Issuer;
+            var sub = User.Claims.FirstOrDefault().Subject.Name;
+
+            var hasUnpublishedTenantQuery = await hostingTenantsCollectionService.Query();
+            var hasWaitingRequest = hasUnpublishedTenantQuery
+                .Where(w => w.Owners.Where(o => o.Iss.Equals(iss) && o.Sub.Equals(sub)).Any()).Any();
+
+            ViewData["WaitingRequestMessage"] = hasWaitingRequest;
             return View();
         }
     }
--- src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/Areas/Admin/Controllers/TenantRegistrationController.cs
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/Areas/Admin/Controllers/TenantRegistrationController.cs
@@ -9,6 +9,10 @@ using HostingModel = TheHorselessNewspaper.Schemas.HostingModel.HostingEntities;
 
 using ContentModel = TheHorselessNewspaper.Schemas.ContentModel.ContentEntities;
 using HorselessNewspaper.RazorClassLibrary.CMS.Default.Areas.Model;
+using HorselessNewspaper.Web.Core.Interfaces.Hosting;
+using TheHorselessNewspaper.HostingModel.Entities.Query;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 
 namespace HorselessNewspaper.RazorClassLibrary.CMS.Default.Areas.Admin.Controllers
 {
@@ -17,19 +21,35 @@ namespace HorselessNewspaper.RazorClassLibrary.CMS.Default.Areas.Admin.Controlle
     public class TenantRegistrationController : Controller
     {
         public IContentCollectionService<IQueryableContentModelOperator<ContentModel.Tenant>, ContentModel.Tenant> tenantCollectionService { get; set; }
+        public IHostingCollectionService<IQueryableHostingModelOperator<HostingModel.Tenant>, HostingModel.Tenant> hostingTenantsCollectionService { get; set; }
         public ITenantInfo CurrentTenant { get; set; }
 
+        ILogger<TenantRegistrationController> logger;
+
         public TenantRegistrationController(
-            IContentCollectionService<IQueryableContentModelOperator<ContentModel.Tenant>,
-            ContentModel.Tenant> tenantCollectionService, ITenantInfo tenantInfo)
+            ILogger<TenantRegistrationController> logger,
+            IContentCollectionService<IQueryableContentModelOperator<ContentModel.Tenant>, ContentModel.Tenant> tenantCollectionService,
+            IHostingCollectionService<IQueryableHostingModelOperator<HostingModel.Tenant>, HostingModel.Tenant> hostingTenantsCollectionService,
+            ITenantInfo tenantInfo)
         {
             this.tenantCollectionService = tenantCollectionService;
+            this.hostingTenantsCollectionService = hostingTenantsCollectionService;
             this.CurrentTenant = tenantInfo;
+            this.logger = logger;
         }
 
         // GET: TenantRegistrationController
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            var iss = User.Claims.FirstOrDefault().Issuer;
+            var sub = User.Claims.FirstOrDefault().Subject.Name;
+
+            var hasUnpublishedTenantQuery = await hostingTenantsCollectionService.Query();
+            var hasWaitingRequest = hasUnpublishedTenantQuery
+                .Where(w => w.Owners.Where(o => o.Iss.Equals(iss) && o.Sub.Equals(sub)).Any()).Any();
+
+            ViewData["WaitingRequestMessage"] = hasWaitingRequest;
+
             return View();
         }
 
@@ -51,14 +71,62 @@ namespace HorselessNewspaper.RazorClassLibrary.CMS.Default.Areas.Admin.Controlle
         // POST: TenantRegistrationController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(TenantRegistrationModel model)
+        public async Task<ActionResult> Create(TenantRegistrationModel model)
         {
             try
             {
+                var newTenant = new HostingModel.Tenant()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow,
+                    DisplayName = model.displayName,
+                    IsPublished = false,
+                    IsSoftDeleted = false,
+                    ObjectId = Guid.NewGuid().ToString(),
+                    Timestamp = BitConverter.GetBytes(DateTime.UtcNow.Ticks),
+                    Owners = new List<HostingModel.Principal>()
+                    {
+                        new HostingModel.Principal()
+                        {
+                            Id = Guid.NewGuid(),
+                            CreatedAt = DateTime.UtcNow,
+                            DisplayName = model.displayName,
+                            IsSoftDeleted = false,
+                            ObjectId = Guid.NewGuid().ToString(),
+                            Timestamp = BitConverter.GetBytes(DateTime.UtcNow.Ticks),
+// aud is not a guarantee
+//                            Aud = User.Claims.Where(w => w.Type.Contains("aud")).FirstOrDefault().Value,
+
+                            // subject/issuer are technically a compound unique key
+                            Iss = User.Claims.FirstOrDefault().Issuer,
+                            Sub = User.Claims.FirstOrDefault().Subject.Name
+
+                        }
+                    },
+                    TenantInfos = new List<HostingModel.TenantInfo>()
+                    {
+                        new HostingModel.TenantInfo()
+                        {
+
+                            Id = Guid.NewGuid(),
+                            CreatedAt = DateTime.UtcNow,
+                            DisplayName = model.displayName,
+                            IsSoftDeleted = false,
+                            ObjectId = Guid.NewGuid().ToString(),
+                            Timestamp = BitConverter.GetBytes(DateTime.UtcNow.Ticks),
+                            Identifier = model.tenantIdentifier,
+                            Name = model.displayName
+                        }
+                    }
+                };
+
+                var insertedTenant = await this.hostingTenantsCollectionService.Create(newTenant);
+
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch(Exception e)
             {
+                logger.LogWarning($"exception inserting new tenant {e.Message}");
                 return View();
             }
         }
