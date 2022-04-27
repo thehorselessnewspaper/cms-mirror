c076fa7faa348f21d8e476aa0c0ceddab634a9fa
devopswizard <devopswizard@wizardcontroller.com>
2022-04-27 16:43:07 -0400

    interface specifications for httpcontext.features model

---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Interfaces/Model/HttpContextFeatureModels/IHorselessSessionFeature.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessNewspaper.Core.Interfaces.Model.HttpContextFeatureModels
+{
+    /// <summary>
+    /// most generic type of feature
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHorselessFeature<T>
+    {
+        public T FeaturePayload { get; set; }
+    }
+
+    /// <summary>
+    /// specialization of features that depend on httpcontext
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHorselessHttpContextFeature<T> : IHorselessFeature<T>
+    {
+
+    }
+
+    public interface IHorselessHttpSessionFeature<T> : IHorselessHttpContextFeature<T>
+    {
+        public string HttpSessionId { get; set; }
+
+        public bool IsAnonymous { get; set; }
+
+        public DateTime SessionStartedAt { get; set; }
+
+        public DateTime SessionLastUpdatedAt { get;  set; }
+    }
+}
