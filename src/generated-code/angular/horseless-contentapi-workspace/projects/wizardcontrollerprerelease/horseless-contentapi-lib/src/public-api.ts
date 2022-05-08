c8718f4ba93a11e1c19f19da7292c179a48c626b
devopswizard <devopswizard@wizardcontroller.com>
2022-05-08 11:56:02 -0400

    package.json updates for contentapi workspace

--- src/generated-code/angular/horseless-contentapi-workspace/projects/wizardcontrollerprerelease/horseless-contentapi-lib/src/public-api.ts
+++ src/generated-code/angular/horseless-contentapi-workspace/projects/wizardcontrollerprerelease/horseless-contentapi-lib/src/public-api.ts
@@ -5,3 +5,8 @@
 export * from './lib/horseless-contentapi-lib.service';
 export * from './lib/horseless-contentapi-lib.component';
 export * from './lib/horseless-contentapi-lib.module';
+
+/*
+ * auto generated horseless content api surface
+ */
+export * from './horseless-contentapi/index'
