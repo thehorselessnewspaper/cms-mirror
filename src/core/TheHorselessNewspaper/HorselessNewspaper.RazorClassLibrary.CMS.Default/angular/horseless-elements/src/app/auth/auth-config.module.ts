c946a4ecadec77b48024650490072c76d6c50ec1
devopswizard <devopswizard@wizardcontroller.com>
2022-05-08 22:43:02 -0400

    horseless elements keycloak integration sanity commit

---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/auth/auth-config.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { AuthModule } from 'angular-auth-oidc-client';
+
+
+@NgModule({
+    imports: [AuthModule.forRoot({
+        config: {
+              authority: 'https://awsdev.ataxlab.com:8443/realms/horseless-infrastructure',
+              redirectUrl: window.location.origin,
+              postLogoutRedirectUri: window.location.origin,
+              clientId: 'please-enter-clientId',
+              scope: 'please-enter-scopes', // 'openid profile offline_access ' + your scopes
+              responseType: 'code',
+              silentRenew: true,
+              useRefreshToken: true,
+              renewTimeBeforeTokenExpiresInSeconds: 30,
+          }
+      })],
+    exports: [AuthModule],
+})
+export class AuthConfigModule {}
