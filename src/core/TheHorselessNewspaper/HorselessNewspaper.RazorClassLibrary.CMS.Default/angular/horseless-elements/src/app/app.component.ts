21c02b40fe93171e84f3158e990c55cd4b9674d5
devopswizard <devopswizard@wizardcontroller.com>
2022-05-14 23:39:14 -0400

    angular ops sanity commit - router experiments

--- src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/app.component.ts
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { DOCUMENT } from '@angular/common';
+import { Location } from '@angular/common';
+import { Router, UrlTree } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +13,26 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 export class AppComponent implements OnInit {
 
-  constructor(public oidcSecurityService: OidcSecurityService) {}
+  constructor(
+    private router: Router,
+    @Inject(DOCUMENT) public document: Document,
+    public oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit() {
     this.oidcSecurityService.checkAuth().subscribe(({isAuthenticated, userData, accessToken}) => {
       console.log('app authenticated', isAuthenticated);
       console.log(`Current access token is '${accessToken}'`);
     });
+  }
+
+  onSwitch(route: string | UrlTree){
+    console.log("window href %s and path name %s and location.origin %s", location.href, location.pathname, location.origin );
+
+    let url = new URL(window.location.href);
+    console.log("url.search %s, url.hash %s url pathname %s",url.search, url.hash, url.pathname);
+    this.router.navigateByUrl(route);
+    // this.location.replaceState("/somepath/" + route);
+    console.log("switching to route %s", route);
   }
 
   login() {
