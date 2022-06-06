46812c8ac100d7426e43ef03bdb1fb46442b5a8b
devopswizard <devopswizard@wizardcontroller.com>
2022-06-05 22:45:25 -0400

    critical functions can connect to rabbit mq and consume messages

--- src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Workflow.AzureFunctions/INGRESS/nuget/CommandDispatcher.cs
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.Core.Workflow.AzureFunctions/INGRESS/nuget/CommandDispatcher.cs
@@ -1,16 +1,26 @@
 using System;
+using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client.Events;
 
 namespace HorselessNewspaper.Core.Workflow.AzureFunctions
 {
     public class CommandDispatcher
     {
         [FunctionName("CommandDispatcher")]
-        public void Run([RabbitMQTrigger("CommandChannel",  ConnectionStringSetting = "localrabbit")]string signal, ILogger log)
+        public void Run([RabbitMQTrigger("CommandChannel", ConnectionStringSetting = "%localrabbit%")] BasicDeliverEventArgs arg, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {signal}");
+            log.LogInformation($"C# Queue trigger function processed message from exchange: {arg.Exchange}");
+
+            log.LogInformation($"C# Queue trigger function routing key: {arg.RoutingKey}");
+
+            log.LogInformation($"C# Queue trigger function body: {Encoding.UTF8.GetString(arg.Body)}");
+
+            log.LogInformation($"C# Queue trigger function cosumer tag: {arg.ConsumerTag}");
+
+            log.LogInformation($"C# Queue trigger function delivery tag: {arg.DeliveryTag}");
         }
     }
 }
