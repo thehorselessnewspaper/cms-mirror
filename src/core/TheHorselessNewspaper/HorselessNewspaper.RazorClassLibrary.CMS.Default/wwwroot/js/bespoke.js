0d86b54f926dcb7be340df909c4b39bc23a845fc
devopswizard <devopswizard@wizardcontroller.com>
2022-06-17 16:54:16 -0400

    dynamically loaded cms view sanity comit

---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/wwwroot/js/bespoke.js
@@ -0,0 +1,28 @@
+﻿
+
+    $.ajaxSetup({
+        'headers': {
+            'viewPhysicalPath': 'streaming',
+            'parentContentCollectionObjectId': 'xxsds',
+        }
+    }
+    );
+
+    $.ajax({
+        url: "/phantom/api/dynamicviewserver/getdynamicview",
+    headers: {
+        viewPhysicalPath: "streaming",
+    parentContentCollectionObjectId: "xxsds",
+    Accept: "text/html; application/json"
+        }
+    }).done(function(data) {
+        $('dynamicView').html(data);
+    });
+
+    $(document).ready(function() {
+
+        $("#button1").click(function () {
+            $("#dynamicView").load("/phantom/api/dynamicviewserver/getdynamicview");
+        });
+
+    });
