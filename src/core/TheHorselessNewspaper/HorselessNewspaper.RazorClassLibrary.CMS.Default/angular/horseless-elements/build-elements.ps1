9bb48ebeadc54f248e9202ead9ada0b6304d2e13
devopswizard <devopswizard@wizardcontroller.com>
2022-06-20 15:01:19 -0400

    npm buildfix sanity commit

---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/build-elements.ps1
@@ -0,0 +1,5 @@
+ng build --project horseless-elements --output-hashing=none
+./pack-elements
+cp projects/wizardcontroller/horseless-tags-library/package.json dist/horseless-elements
+cp dist/horseless-elements/elements.js ../../wwwroot/js/horseless-elements/horseless-tags-library
+cp dist/horseless-elements/styles.css ../../wwwroot/js/horseless-elements/horseless-tags-library
---
+++ src/core/TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/angular/horseless-elements/pack-elements.ps1
@@ -0,0 +1,1 @@
+get-content ./dist/horseless-elements/runtime.js, ./dist/horseless-elements/polyfills.js, ./dist/horseless-elements/main.js | set-content ./dist/horseless-elements/elements.js
