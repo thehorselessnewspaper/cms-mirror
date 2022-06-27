77b5fe22e6942394d94a2d2a82732d6f0aa426de
devopswizard <devopswizard@wizardcontroller.com>
2022-06-27 10:50:12 -0400

    horseless odata api ops

--- src/generated-code/angular/horseless-odataapi-workspace/projects/horselessnewspaper/horseless-hosting-odataapi/src/public-api.ts
+++ src/generated-code/angular/horseless-odataapi-workspace/projects/horselessnewspaper/horseless-hosting-odataapi/src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of horseless-hosting-odataapi
  */
 
+
+
 export * from './lib/horseless-hosting-odataapi.service';
 export * from './lib/horseless-hosting-odataapi.component';
 export * from './lib/horseless-hosting-odataapi.module';
+
