c25fb1c4881f93c066beee1e2bfa869f6ca09965
devopswizard <devopswizard@wizardcontroller.com>
2022-07-21 14:37:56 -0400

    autoinclude updates

--- src/entities/Model.Core.Taxa/TheHorselessNewspaper.Schemas.HostingModel/EntityPartials/Content/THLNPContentContext.cs
+++ src/entities/Model.Core.Taxa/TheHorselessNewspaper.Schemas.HostingModel/EntityPartials/Content/THLNPContentContext.cs
@@ -133,11 +133,11 @@ namespace TheHorselessNewspaper.Schemas.ContentModel.ContentEntities
             //builder.Entity<HorselessTenantInfo>().IsMultiTenant();
 
             builder.Entity<Tenant>()
-                .Navigation(n => n.Accounts).AutoInclude();
-
-            builder.Entity<Tenant>()
                 .Navigation(n => n.Owners).AutoInclude();
 
+
+            builder.Entity<Tenant>()
+                .Navigation(n => n.Accounts).AutoInclude();
             builder.Entity<Tenant>()
                 .Navigation(n => n.AccessControlEntries).AutoInclude();
 
@@ -148,20 +148,15 @@ namespace TheHorselessNewspaper.Schemas.ContentModel.ContentEntities
                 .Navigation(n => n.TenantIdentifierStrategy).AutoInclude();
 
 
-            builder.Entity<ContentCollection>()
-                .Navigation(n => n.ChildContentCollections).AutoInclude();
 
             builder.Entity<ContentCollection>()
                 .Navigation(n => n.HorselessViews).AutoInclude();
 
-            builder.Entity<ContentCollection>()
-                .Navigation(n => n.Owners).AutoInclude();
 
             builder.Entity<Principal>()
             .Navigation(n => n.HorselessSessions).AutoInclude();
 
-            builder.Entity<Principal>()
-                .Navigation(n => n.Accounts).AutoInclude();
+
 
 
 
