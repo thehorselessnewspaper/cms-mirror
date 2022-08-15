fa2b8a384daeee8be8e018df8ebfa526deee35fe
devopswizard <devopswizard@wizardcontroller.com>
2022-08-14 22:56:06 -0400

    npm ops commit

--- src/core/horseless-elements/angular/horseless-elements/build-elements.ps1
+++ src/core/horseless-elements/angular/horseless-elements/build-elements.ps1
@@ -1,6 +1,8 @@
+$wwwRoot =  ../../../TheHorselessNewspaper/HorselessNewspaper.RazorClassLibrary.CMS.Default/wwwroot/js/horseless-elements/horseless-tags-library/
+
 ng build --project horseless-elements --output-hashing=none
 ./pack-elements
-mkdir ../../wwwroot/js/horseless-elements/horseless-tags-library
+mkdir $wwwRoot
 cp projects/wizardcontroller/horseless-tags-library/package.json dist/horseless-elements
-cp dist/horseless-elements/elements.js ../../wwwroot/js/horseless-elements/horseless-tags-library/
-cp dist/horseless-elements/styles.css ../../wwwroot/js/horseless-elements/horseless-tags-library/
+cp dist/horseless-elements/elements.js $wwwRoot
+cp dist/horseless-elements/styles.css $wwwRoot
